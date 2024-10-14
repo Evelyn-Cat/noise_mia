184688ea47ac72586a045d5150e189698a1934dd
Evelyn-Cat <qin.yang@uconn.edu>
2024-10-14 12:02:55 -0400

    mod

--- noise_mia.py
+++ noise_mia.py
@@ -9,7 +9,7 @@ for key, values in search_range.items():
 all_combinations = list(product(*expanded_items))
 print(all_combinations[1])
 
-def main(epsilon):
+def main(epsilon, sensitivity):
     param_dict = {}
     for combination in all_combinations:
         for i, key in enumerate(list(search_range.keys())):
@@ -48,6 +48,8 @@ if __name__ == '__main__':
 
     import sys
     epsilon = sys.argv[1]
-    print(epsilon)
-    main(epsilon)
+    sensitivity = sys.argv[2]
+    print(epsilon, sensitivity)
+    main(epsilon, sensitivity)
+
 
