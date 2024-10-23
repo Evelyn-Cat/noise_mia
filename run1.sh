dcb9af9eaf3cd49dda9341c46c98502a70d7b06b
Evelyn-Cat <qin.yang@uconn.edu>
2024-10-23 13:57:00 -0400

    mod.ready_to_run

--- run1.sh
+++ run1.sh
@@ -1,10 +1,9 @@
-python noise_mia_14.py 1.0 0.05 5
+python noise_mia.py 1.0 0.05 5
 wait
-python noise_mia_14.py 1.0 0.05 10
+python noise_mia.py 1.0 0.05 10
 wait
-python noise_mia_14.py 1.0 0.05 20
+python noise_mia.py 1.0 0.05 20
 wait
-python noise_mia_14.py 1.0 0.05 50
+python noise_mia.py 1.0 0.05 50
 wait
-python noise_mia_14.py 1.0 0.05 100
-wait
+python noise_mia.py 1.0 0.05 100
--- run3.sh
+++ run3.sh
@@ -1,10 +1,9 @@
-python noise_mia_14.py 0.6 0.05 1
+python noise_mia.py 0.6 0.05 1
 wait
-python noise_mia_14.py 0.7 0.05 1
+python noise_mia.py 0.7 0.05 1
 wait
-python noise_mia_14.py 0.8 0.05 1
+python noise_mia.py 0.8 0.05 1
 wait
-python noise_mia_14.py 0.9 0.05 1
+python noise_mia.py 0.9 0.05 1
 wait
-python noise_mia_14.py 1.0 0.05 1
-wait
+python noise_mia.py 1.0 0.05 1
