0adea6a7dd5d19a7e0911718787a156f567282b2
Evelyn-Cat <qin.yang@uconn.edu>
2024-10-31 15:05:05 -0400

    mod.2024.10.31

--- noise_auditing/auditing/acc_cv.sh
+++ noise_auditing/auditing/acc_cv.sh
@@ -1,4 +1,4 @@
-list1=("fmnist" "p100")
+list1=("p100") # "fmnist" epoch 25; p100 epoch 100
 list2=("2f" "lr")
 # l2=(0.5 1 2 3 4 5) # 1
 # noise_type=("gaussian")
@@ -9,7 +9,7 @@ for param in "${list1[@]}"; do
     for value in "${list2[@]}"; do
         for noise_param in "${noise_params[@]}"; do
             # echo "Processing $param with $value"
-            python acc_cv.py -dataset ${param} -model ${value} -l2_norm_clip 1 -noise_type gaussian -noise_params ${noise_param} -microbatches 1 -epochs 20 -batch_size 250 > log.$param.$value.$noise_param.log
+            python acc_cv.py -dataset ${param} -model ${value} -l2_norm_clip 1 -noise_type gaussian -noise_params ${noise_param} -microbatches 1 -epochs 100 -batch_size 250 > log.$param.$value.$noise_param.log
         done
     done
 done
