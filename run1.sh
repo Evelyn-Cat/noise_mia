c2feedfaa9d7f6b2a5a46183bd239bee65426596
Evelyn-Cat <qin.yang@uconn.edu>
2024-10-31 16:25:39 -0400

    mod.add

---
+++ run1.sh
@@ -0,0 +1,59 @@
+python noise_mia.py 1.0 0.15 5 g
+wait
+python noise_mia.py 1.0 0.15 10 g
+wait
+python noise_mia.py 1.0 0.15 20 g
+wait
+python noise_mia.py 1.0 0.15 50 g
+wait
+python noise_mia.py 1.0 0.15 100 g
+wait
+python noise_mia.py 1.0 0.2 5 g
+wait
+python noise_mia.py 1.0 0.2 10 g
+wait
+python noise_mia.py 1.0 0.2 20 g
+wait
+python noise_mia.py 1.0 0.2 50 g
+wait
+python noise_mia.py 1.0 0.2 100 g
+wait
+python noise_mia.py 0.1 0.15 1 g
+wait
+python noise_mia.py 0.2 0.15 1 g
+wait
+python noise_mia.py 0.3 0.15 1 g
+wait
+python noise_mia.py 0.4 0.15 1 g
+wait
+python noise_mia.py 0.5 0.15 1 g
+wait
+python noise_mia.py 0.6 0.15 1 g
+wait
+python noise_mia.py 0.7 0.15 1 g
+wait
+python noise_mia.py 0.8 0.15 1 g
+wait
+python noise_mia.py 0.9 0.15 1 g
+wait
+python noise_mia.py 1.0 0.15 1 g
+wait
+python noise_mia.py 0.1 0.2 1 g
+wait
+python noise_mia.py 0.2 0.2 1 g
+wait
+python noise_mia.py 0.3 0.2 1 g
+wait
+python noise_mia.py 0.4 0.2 1 g
+wait
+python noise_mia.py 0.5 0.2 1 g
+wait
+python noise_mia.py 0.6 0.2 1 g
+wait
+python noise_mia.py 0.7 0.2 1 g
+wait
+python noise_mia.py 0.8 0.2 1 g
+wait
+python noise_mia.py 0.9 0.2 1 g
+wait
+python noise_mia.py 1.0 0.2 1 g
