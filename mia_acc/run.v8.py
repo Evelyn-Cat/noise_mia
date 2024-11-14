685ca6316cb76b2d496448bfdcb1fd8e09b2e9d3
Evelyn-Cat <qin.yang@uconn.edu>
2024-11-14 16:53:53 -0500

    mod.run.gaussian.repeat.10

--- mia_acc/run.v8.py
+++ mia_acc/run.v8.py
@@ -1,11 +1,13 @@
 version="v8"
 
-suffix_epochs=50
+# suffix_epochs=50
 savefolder="save_folder"
-prefix_noise_type="mg"
+prefix_noise_type="gaussian"
+repeat=10
 
 for dataset in ["p100"]:
     for model in ["2f"]:
-        for prefix_noise_params in list(range(1)):
-            print(f"python run_mia_acc.{version}.py {dataset} {model} {suffix_epochs} {savefolder} {prefix_noise_type} {prefix_noise_params} > logs/{version}.{prefix_noise_type}.{prefix_noise_params}.{dataset}.{model}.T_{suffix_epochs}.log")
-            print("wait")
+        for suffix_epochs in [1000, 10, 20, 30, 40, 50, 100, 200, 500]:
+            for prefix_noise_params in [0.01, 0.05, 0.1, 0.5, 1, 2, 5, 10, 100, 1000, 10000]:
+                print(f"python run_mia_acc.{version}.py {dataset} {model} {suffix_epochs} {savefolder} {prefix_noise_type} {prefix_noise_params} > logs/rp_{repeat}.{version}.{prefix_noise_type}.{prefix_noise_params}.{dataset}.{model}.T_{suffix_epochs}.log")
+                print("wait")
